6707abbe05593e6ea807108b43c2a2f8829fd1d5
PTH922 <pth922@gmail.com>
2024-05-26 17:13:10 +0900

    Clonecoding - complete

---
+++ Assets/Script/Pipes.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    public float Speed = 5f;
+    private float leftEdge;
+
+    private void Start()
+    {
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
+    }
+    private void Update()
+    {
+        transform.position += Vector3.left * Speed * Time.deltaTime; // consistantly moving
+        if(transform.position.x < leftEdge)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
